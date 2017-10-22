08e9f78b1ecf7f69b3663e25853e0fe034199c88
brendan carlquist <carlquistb@gmail.com>
2017-10-22 12:16:24 -0700

    b

--- website/getGender.php
+++ website/getGender.php
@@ -11,13 +11,10 @@ include("LighthouseCommon.php");
 $results = executeSelect("select GenderName from Gender");
 
 $Array = array();
-if ($result = $mysqli->query("SELECT * FROM phase1")) {
-
-    while($row = $result->fetch_array(MYSQL_ASSOC)) {
-        $Array[] = $row;
-    }
-    echo json_encode($myArray);
+while($row = $result->fetch_array(MYSQL_ASSOC)) {
+    $Array[] = $row;
 }
+echo json_encode($Array);
 
 $result->close();
 $mysqli->close();
