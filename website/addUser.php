a0ffa714a8641d3c6b083af93a7fa4f3f3533d2b
brendan carlquist <carlquistb@gmail.com>
2017-10-22 10:39:10 -0700

    beginning of website work.

---
+++ website/addUser.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iguest
+ * Date: 10/22/17
+ * Time: 10:27 AM
+ */
+ include("common.php");
+ executeInsert("")
+
+ header("Location: website/addUser.php");
