3bde4b9d39dbf7d7ed7dfdbb90dd4b8ebe6b6a11
brendan carlquist <carlquistb@gmail.com>
2017-10-22 08:56:54 -0700

    deleted phpServer folder.

--- phpServer/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: carlquistb
- * Date: 10/22/2017
- * Time: 4:51 AM
- */
-?>
-<HTML><body>this will be pretty <? for($i = 0; $i < 3; $i++) { ?>great!<?php } ?></body></HTML>
