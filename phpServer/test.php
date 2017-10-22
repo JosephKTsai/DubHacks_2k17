11ead499f4d379febd62fc34d4a6a0725b02085a
Brendan A Carlquist <carlquistb@gmail.com>
2017-10-22 09:02:16 -0700

    to stash.

--- phpServer/test.php
+++ phpServer/test.php
@@ -5,5 +5,18 @@
  * Date: 10/22/2017
  * Time: 4:51 AM
  */
-?>
-<HTML><body>this will be pretty <? for($i = 0; $i < 3; $i++) { ?>great!<?php } ?></body></HTML>
+$servername = 'localhost';//"13.0.4001.0";
+$username = "publicuser";
+$password = "public";
+$dbname = "Lighthouse";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
